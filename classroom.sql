54845b6b60680403a02e1a71482cd72377916ced
sujan <budhasujan3@gmail.com>
2025-02-06 15:36:58 +0545

    learning SQL

---
+++ classroom.sql
@@ -0,0 +1,21 @@
+
+CREATE DATABASE college;
+
+USE college;
+
+CREATE TABLE student
+(	
+	id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+
+INSERT INTO student VALUES
+(1, 'Sujan', 24),
+(2, 'Binita', 34),
+(3, 'Sudip', 21);
+
+SELECT * FROM student;
+
+insert into student values(4, 'Namita', 23);
